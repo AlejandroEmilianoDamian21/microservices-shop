ae290c89a69e3ed1abf6fabeebf1f726f9a20300
Emiliano Alejandro <alejandroemiliano21@gmail.com>
2025-01-13 14:00:25 -0300

    Implementando API-Gateway

--- orders-service/src/main/java/com/tikidev/orders_service/models/dtos/OrderResponse.java
+++ orders-service/src/main/java/com/tikidev/orders_service/models/dtos/OrderResponse.java
@@ -3,5 +3,4 @@ package com.tikidev.orders_service.models.dtos;
 import java.util.List;
 
 public record OrderResponse(Long id, String orderNumber, List<OrderItemsResponse> orderItemsResponse) {
-
 }
